b5bbeee7ab5bd39eed8af2bcbdcb9dd4599ad8b3
jalgelind <jalgelind@gmail.com>
2010-01-29 21:04:20 +0100

    Added sprite and position to gameobject

--- gameobject.py
+++ gameobject.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
+import pygame
 
 class GameObject:
-    def __init__(self, pos):
+    def __init__(self, pos, sprite):
+        Sprite.__init__(self)
         self.pos = pos
-
+        self.sprite = sprite
+        
+    def update(self):
+        pass
     
+    def draw(self, canvas):
+        canvas.blit(self.sprite, self.pos)

6ed7d77d515c4a5ceefda717a4221a1c5bdd22a7
jalgelind <jalgelind@gmail.com>
2010-01-29 19:09:38 +0100

    Changed indenting style to use spaces. (4 spaces)

--- game.py
+++ game.py
@@ -1,4 +1,8 @@
 import sys, pygame
+
+def Init():
+    pass
+
 pygame.init()
 
 size = width, height = 320, 240
@@ -11,15 +15,15 @@ ball = pygame.image.load("data/ball.png")
 ballrect = ball.get_rect()
 
 while 1:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit()
-	
-	ballrect = ballrect.move(speed)
-	if ballrect.left < 0 or ballrect.right > width:
-		speed[0] = -speed[0]
-	if ballrect.top < 0 or ballrect.bottom > height:
-		speed[1] = -speed[1]
-	
-	screen.fill(black)
-	screen.blit(ball, ballrect)
-	pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+    
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+    
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()

7e006a8e70c241194cf83dd34c0260f9171caa74
andsve <sweetfish@gmail.com>
2010-01-30 19:44:40 +0100

    YPAOE

--- player.py
+++ player.py
@@ -14,15 +14,15 @@ class Player(gameobject.GameObject):
         #create animations
         player_walk_left = animation.new_animation("data/bw_guy_walk","png",1,8,[0,1])
         player_walk_right = animation.new_animation("data/bw_guy_walk_r","png",1,8,[0,1])
-        
+
         #player_walk_left.play()
         self.image = player_walk_left.sprite.image
-        
+
         self.animations["player_walk_left"] = player_walk_left
         self.animations["player_walk_right"] = player_walk_right
-        
+
         self.current_animation = player_walk_left
-        
+
         self.look_dir = 0 # 0 = right, 1 = left
         self.bw_image = pygame.image.load("data/bw_player16.png")
         self.red_image = pygame.image.load("data/red_player16.png")
@@ -38,8 +38,9 @@ class Player(gameobject.GameObject):
         gameobject.GameObject.update(self, camera_pos)
         for anim in self.animations.keys():
             self.animations[anim].update(dt)
-    
-    def set_color()
+
+    def set_color():
+        pass
 
     def draw(self, canvas):
         if (self.look_dir == 0):
@@ -47,7 +48,7 @@ class Player(gameobject.GameObject):
             # find correct color
             body_image = self.bw_image
             cloth_image = self.red_image
-            
+
             if not self.current_animation.playing:
                 self.current_animation.play()
             self.current_animation = self.animations["player_walk_right"]
@@ -56,7 +57,7 @@ class Player(gameobject.GameObject):
             # find correct color
             body_image = self.bw_image_r
             cloth_image = self.red_image_r
-            
+
             if not self.current_animation.playing:
                 self.current_animation.play()
             self.current_animation = self.animations["player_walk_left"]

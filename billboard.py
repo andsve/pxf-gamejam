2e17fb299486093304334a67b27a2f835b963b8b
JhonnyG <jhonny.goransson@gmail.com>
2010-01-31 01:46:45 +0100

    okay

--- billboard.py
+++ billboard.py
@@ -10,16 +10,22 @@ class Billboard:
         self.pos = pos
         self.draw_pos = None
         self.speed = speed
-        self.inv_speed = speed/100.
+        self.inv_speed = speed/10.
     
     def draw(self,canvas):
         if not self.is_animated:
             canvas.blit(self.image, self.draw_pos, None, pygame.BLEND_MAX)
         pass
     
-    def update(self,cam_pos,dt):
+    def update(self,cam_pos,dt):        
         self.draw_pos = (self.pos[0]-cam_pos.x*self.inv_speed,
                          self.pos[1]-cam_pos.y*self.inv_speed)
         self.pos[0] * dt
         self.pos[1] * dt
+        
+        print self.draw_pos
+        
+        if self.draw_pos[0] < 0:
+            print "hknk"
+        
         pass
